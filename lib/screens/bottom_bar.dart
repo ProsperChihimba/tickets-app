2cf3e3c8d72f22032b51b05098282e26815c08ec
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-13 01:44:43 +0400

    add: buttom bar widget

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtomBar extends StatefulWidget {
+  const ButtomBar({super.key});
+
+  @override
+  State<ButtomBar> createState() => _ButtomBarState();
+}
+
+class _ButtomBarState extends State<ButtomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
