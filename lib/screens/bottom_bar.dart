c879684e32bef826aab05c0dd49364becb3b210f
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-13 13:01:33 +0400

    add: bottom navigation bar initial design

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class ButtomBar extends StatefulWidget {
@@ -8,8 +9,60 @@ class ButtomBar extends StatefulWidget {
 }
 
 class _ButtomBarState extends State<ButtomBar> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tickets"),
+      ),
+      body: Center(child: _widgetOptions[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            label: "Home",
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+            label: "search",
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+            label: "ticket",
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+            label: "person",
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+          ),
+        ],
+      ),
+    );
   }
 }
