f36b2afbf966b51f76569e6e4e86bd9e95e28f8f
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-21 04:48:24 +0400

    Add: search screen widget

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tickets/utils/app_styles.dart';
+
+class SearchScree extends StatelessWidget {
+  const SearchScree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
