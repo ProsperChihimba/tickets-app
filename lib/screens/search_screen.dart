55c8418895a99bf6ce9acda6443526857118c922
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-21 05:34:35 +0400

    Add: search screen tickets & hotel row

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -8,6 +8,7 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
@@ -16,9 +17,44 @@ class SearchScreen extends StatelessWidget {
             vertical: AppLayout.getHeight(20)),
         children: [
           Gap(AppLayout.getHeight(40)),
+          // Page main title
           Text(
             "What are\nyou looking for?",
             style: Styles.headLineStyle.copyWith(fontSize: 35),
+          ),
+          Gap(AppLayout.getHeight(20)),
+          //
+          FittedBox(
+            child: Container(
+              padding: const EdgeInsets.all(3.5),
+              child: Row(
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    child: Center(child: Text("Airline tickets")),
+                    width: size.width * .44,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.white),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    child: Center(child: Text("Hotels")),
+                    width: size.width * .44,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(
+                            right: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.transparent),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                  color: const Color(0xFFF4F7FD)),
+            ),
           )
         ],
       ),
